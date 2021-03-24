ca707036e052416d963ae0019e0a0f1c9aa7b775
Gustavo <gustavocontas0@gmail.com>
2021-03-24 18:49:24 -0300

    Index

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const database_redis = require('./redis');
+const database = require('./database');
+
+
+//database_redis.setKey(9, "Produto: Monitor JS, preco: 800")
+//batabase_redis.setTime(10, 30, "Produto: Gabinete Aigo, Preco: 350")
+//batabase_redis.getKey(1)
+//database_redis.delKey
+
+//database.getData(4)
+//database.inserirProduto("Mouse Y", 68)
+//database.updateProduto(90, "Mouse X")
+//database.delProduto(2)
+
+
